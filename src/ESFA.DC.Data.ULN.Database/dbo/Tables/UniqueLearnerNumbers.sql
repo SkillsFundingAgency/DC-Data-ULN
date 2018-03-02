1f460ddf7afa08815584736445b4e902cb3e21fc
Peter Chamley <peter@chamley.name>
2018-03-02 17:05:11 +0000

    Add Db Project. Create Dbo Table and Staging Table. Add SP to move data from Staging to dbo. Add SSIS Package to Load data into Staging then Process Data.

---
+++ src/ESFA.DC.Data.ULN.Database/dbo/Tables/UniqueLearnerNumbers.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[UniqueLearnerNumbers] (
+    [ULN] BIGINT NOT NULL,
+	[Created_On] [datetime] CONSTRAINT [def_dboUniqueLearnerNumbers_CreatedOn] DEFAULT (GETUTCDATE()) NOT NULL,
+	[Created_By] [nvarchar](256) CONSTRAINT [def_DboUniqueLearnerNumbers_CreatedBy] DEFAULT (SUSER_SNAME()) NOT NULL,
+	[Modified_On] [datetime] CONSTRAINT [def_dboUniqueLearnerNumbers_ModifiedOn] DEFAULT (GETUTCDATE())  NOT NULL,
+	[Modified_By] [nvarchar](256) CONSTRAINT [def_dboUniqueLearnerNumbers_ModifiedBy] DEFAULT (SUSER_SNAME())  NOT NULL,
+    CONSTRAINT [PK_UniqueLearnerNumbers] PRIMARY KEY CLUSTERED ([ULN] ASC)
+);
+
